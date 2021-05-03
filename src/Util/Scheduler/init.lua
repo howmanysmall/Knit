4e3dc45fa604694190623b6a756577fa37786ef6
howmanysmall <lhamil29@bruinmail.slcc.edu>
2021-05-03 12:41:12 -0600

    Updated Scheduler.

--- src/Util/Scheduler/init.lua
+++ src/Util/Scheduler/init.lua
@@ -1,2 +1,8 @@
+local SchedulerSafe = require(script.Safe)
+local SchedulerUnsafe = require(script.Unsafe)
 local SHOULD_USE_UNSAFE = false
-return SHOULD_USE_UNSAFE and require(script.Unsafe) or require(script.Safe)
+
+return setmetatable({
+	Safe = SchedulerSafe;
+	Unsafe = SchedulerUnsafe;
+}, SHOULD_USE_UNSAFE and {__index = SchedulerUnsafe} or {__index = SchedulerSafe})
